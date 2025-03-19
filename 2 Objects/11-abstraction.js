6c7dbf82d4f039fcef6a0799314000d2989f6e61
asimov <seyranmustafa4@gmail.com>
2025-03-19 23:01:16 +0300

    p

--- 2 Objects/11-abstraction.js
+++ 2 Objects/11-abstraction.js
@@ -2,10 +2,21 @@
 function Programmer(name,preferredLanguage){
     this.name=name;
     this.preferredLanguage=preferredLanguage;
+    //Public method
     this.writeCode=function(){
         console.log(`${this.name} writes ${this.preferredLanguage} code.`);
     };
     this.drinkCoffee=function(){
         console.log(`${this.name} drinks coffee.`);
     };
+
+    //Private method
+    const drinkCoffee=function(){
+        console.log(`${this.name} drinks coffee.`);
+    }
+    //Public method
+    this.startDay=function(){
+        drinkCoffee();
+    }
+    
 }
