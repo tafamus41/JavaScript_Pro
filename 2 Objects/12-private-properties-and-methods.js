24431555835b8aefdd3cfa33bcdb355913c37eae
asimov <seyranmustafa4@gmail.com>
2025-03-20 00:52:00 +0300

    p

--- 2 Objects/12-private-properties-and-methods.js
+++ 2 Objects/12-private-properties-and-methods.js
@@ -9,19 +9,37 @@
 // const innerFunction=example();
 // innerFunction();
 
-function makeFunction() {
-    let privateNum=0;
-    function privateIncrement() {   
-        privateNum++;       
-    }
-    return {
-        logNum:()=>console.log(privateNum),
-        increment:()=>{privateIncrement();
-            console.log("Incremented");}
+// function makeFunction() {
+//     let privateNum=0;
+//     function privateIncrement() {   
+//         privateNum++;       
+//     }
+//     return {
+//         logNum:()=>console.log(privateNum),
+//         increment:()=>{privateIncrement();
+//             console.log("Incremented");}
 
-    }
+//     }
+// }
+// const{logNum,increment}=makeFunction();
+// logNum();
+// increment();
+// logNum();
+
+function Programmer(name, preferredLanguage) {
+    let privateName = name;
+    this.preferredLanguage = preferredLanguage;
+    this.writeCode = function () {
+        console.log(`Code in ${preferredLanguage}`);
+    };
+    let drinkCoffee = function () {
+        console.log("Gulp...");
+    };
+    this.startDay = function () {
+        drinkCoffee();
+    };
 }
-const{logNum,increment}=makeFunction();
-logNum();
-increment();
-logNum();
+
+const programmer = new Programmer("John", "JavaScript");
+programmer.writeCode();
+programmer.startDay();
