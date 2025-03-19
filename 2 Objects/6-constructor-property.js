ad40c2cded246b2a146dc8fe339272f8f813b319
asimov <seyranmustafa4@gmail.com>
2025-03-19 04:59:55 +0300

    p

---
+++ 2 Objects/6-constructor-property.js
@@ -0,0 +1,14 @@
+function Programmer(name,preferredLanguage) {
+    this.name=name;
+    this.preferredLanguage=preferredLanguage;
+    this.writeCode=function(){
+        console.log(`${this.name} writes ${this.preferredLanguage} code.`);
+    };
+    this.drinkCoffee=function(){
+        console.log(`${this.name} drinks coffee.`);        
+    };
+    }
+    
+    const newProgrammer=new Programmer("John","JavaScript");
+    console.log(newProgrammer.constructor);
+    newProgrammer.writeCode();
