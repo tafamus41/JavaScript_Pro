a7000495cc0b98bf5e3dee29d954c7ab76325857
asimov <seyranmustafa4@gmail.com>
2025-03-20 01:09:56 +0300

    p

--- 2 Objects/12-private-properties-and-methods.js
+++ 2 Objects/12-private-properties-and-methods.js
@@ -67,7 +67,7 @@ function BankAccount(initialBalance) {
         if (isValidAmount(amount)) {
             if (amount <= balance) {
                 balance -= amount;
-                console.log(`Withdrew: $${amount}`);
+                console.log(`Withdraw: $${amount}`);
             } else {
                 console.log('Insufficient funds');
             }
