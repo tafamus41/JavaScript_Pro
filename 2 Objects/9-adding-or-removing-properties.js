a68c35a97c66eb1d4d26b780f34a36c53b02a48d
asimov <seyranmustafa4@gmail.com>
2025-03-19 22:27:39 +0300

    p

--- 2 Objects/9-adding-or-removing-properties.js
+++ 2 Objects/9-adding-or-removing-properties.js
@@ -1,17 +1,25 @@
-
-const person={name:"Adam"};
+const person = { name: "Adam" };
 console.log(person);
 
-person.favoriteFood="Pizza";
+person.favoriteFood = "Pizza";
 console.log(person);
 
-person["favoriteIceCream"]="Vanilla";
+person["favoriteIceCream"] = "Vanilla";
 console.log(person);
 
 delete person.favoriteIceCream;
 console.log(person);
 
-person.eat=function(){
-    console.log(`${this.name} eats ${this.favoriteFood}.`);
-}
+person.eat = function () {
+  console.log(`${this.name} eats ${this.favoriteFood}.`);
+};
 person.eat();
+
+function GroceryItem(name, quantity) {
+  this.name = name;
+  this.quantity = quantity;
+  this.display = function () {
+    console.log(`${this.name} x ${this.quantity}`);
+  };
+}
+const newItem = new GroceryItem("banana", 2);
