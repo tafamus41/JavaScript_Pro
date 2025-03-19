fcbae7b584d7bbbaa94ee67f6ab6a35cd79bd180
asimov <seyranmustafa4@gmail.com>
2025-03-19 22:58:00 +0300

    p

---
+++ 2 Objects/11-abstraction.js
@@ -0,0 +1,11 @@
+
+function Programmer(name,preferredLanguage){
+    this.name=name;
+    this.preferredLanguage=preferredLanguage;
+    this.writeCode=function(){
+        console.log(`${this.name} writes ${this.preferredLanguage} code.`);
+    };
+    this.drinkCoffee=function(){
+        console.log(`${this.name} drinks coffee.`);
+    };
+}
