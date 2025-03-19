f37334020b4d34b3b5f75a3d0d88e9d5e84875b8
asimov <seyranmustafa4@gmail.com>
2025-03-19 23:30:49 +0300

    p

--- 2 Objects/11-abstraction.js
+++ 2 Objects/11-abstraction.js
@@ -1,22 +1,28 @@
-
-function Programmer(name,preferredLanguage){
-    this.name=name;
-    this.preferredLanguage=preferredLanguage;
-    //Public method
-    this.writeCode=function(){
-        console.log(`${this.name} writes ${this.preferredLanguage} code.`);
-    };
-     //Private method
-    const drinkCoffee=function(){
-        console.log(`${this.name} drinks coffee.`);
-    }.bind(this);
-    //Public method
-    this.startDay=function(){
-        drinkCoffee();
-    }
-    
+function Programmer(name, preferredLanguage) {
+  this.name = name;
+  this.preferredLanguage = preferredLanguage;
+  //Public method
+  this.writeCode = function () {
+    console.log(`${this.name} writes ${this.preferredLanguage} code.`);
+  };
+  //Private method
+  const drinkCoffee = function () {
+    console.log(`${this.name} drinks coffee.`);
+  }.bind(this);
+  //Public method
+  this.startDay = function () {
+    drinkCoffee();
+  };
 }
 
-const programmer=new Programmer("John","JavaScript");
+const programmer = new Programmer("John", "JavaScript");
 programmer.writeCode();
 programmer.startDay();
+
+function GroceryItem(name, quantity) {
+  this.name = name;
+  this.quantity = quantity;
+  this.display = function () {
+    console.log(`${this.name} x ${this.quantity}`);
+  };
+}
