a93632c47dd8ad9c740eeca4e1fc0310e3ce5c64
asimov <seyranmustafa4@gmail.com>
2025-03-19 22:43:02 +0300

    p

--- 2 Objects/10-enumerating-properties.js
+++ 2 Objects/10-enumerating-properties.js
@@ -1,28 +1,34 @@
-
-let numbers=[1,2,3,4,5];
-for(const element of numbers){
-    console.log(element);
+let numbers = [1, 2, 3, 4, 5];
+for (const element of numbers) {
+  console.log(element);
 }
 
-const dog={
-    name:"Fido",
-    age:2,
-    eyeColor:"blue"
+const dog = {
+  name: "Fido",
+  age: 2,
+  eyeColor: "blue",
 };
-for(const property in dog){
-    console.log(dog[property]);
+for (const property in dog) {
+  console.log(dog[property]);
 }
-const keys=Object.keys(dog);
-for(const key of keys){
-    console.log(key);
-}
-
-const values=Object.values(dog);
-for(const value of values){
-    console.log(value);
+const keys = Object.keys(dog);
+for (const key of keys) {
+  console.log(key);
 }
 
-const entries=Object.entries(dog);
-for(const entry of entries){
-    console.log(`Key: ${entry[0]}, Value: ${entry[1]}`);
+const values = Object.values(dog);
+for (const value of values) {
+  console.log(value);
+}
+
+const entries = Object.entries(dog);
+for (const entry of entries) {
+  console.log(`Key: ${entry[0]}, Value: ${entry[1]}`);
+}
+function GroceryItem(name, quantity) {
+  this.name = name;
+  this.quantity = quantity;
+  this.display = function () {
+    console.log(`${this.name} x ${this.quantity}`);
+  };
 }
