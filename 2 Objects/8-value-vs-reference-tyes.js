088b5c88e09e57b6933d501c3b1dc8429b7e8a2a
asimov <seyranmustafa4@gmail.com>
2025-03-19 05:32:38 +0300

    p

--- 2 Objects/8-value-vs-reference-tyes.js
+++ 2 Objects/8-value-vs-reference-tyes.js
@@ -1,5 +1,14 @@
+
+// Primitive Types: Number, String, Boolean, Null, Undefined, Symbol Passed by value(by copy)
 let a=10;
 let b=a;
 a=20;
+console.log(a);
+console.log(b);
+
+// Reference Types: Objects, Arrays, Functions, Date, RegExp, Error, Map, Set, WeakMap, WeakSet Passed by reference
+a={value:20};
+b=a;
+a.value=100;
 console.log(a);
 console.log(b);
