a77272271b18186598e9e5d5eed3d15f08c68cec
asimov <seyranmustafa4@gmail.com>
2025-03-21 01:59:22 +0300

    p

---
+++ 2 Objects/13-getters-and-setters.js
@@ -0,0 +1,43 @@
+// Lesson
+function Programmer(name, preferredLanguage) {
+    // Private property
+    let privateName = name;
+
+    Object.defineProperties(this, {
+        'name': {
+            get: function() {
+                return privateName;
+            },
+            set: function(newName) {
+                if (!newName) {
+                    console.log('Name cannot be empty');
+                    return;
+                }
+                privateName = newName;
+            }
+        }
+    });
+
+    // Public property
+    this.preferredLanguage = preferredLanguage;
+
+    // Public method
+    this.writeCode = function() {
+        console.log(`${privateName} codes in ${this.preferredLanguage}`);
+    }
+
+    // Private method
+    let drinkCoffee = function() {
+        console.log('Gulp...');
+    }
+
+    // Public method that uses a closure
+    this.startDay = function() {
+        drinkCoffee();
+    }
+}
+
+const programmer = new Programmer('Alice', 'JavaScript');
+console.log(programmer.name);
+programmer.name = '';
+console.log(programmer.name);
