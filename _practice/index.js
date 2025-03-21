23fce2909dfb12398983d139053d049597235ce2
asimov <seyranmustafa4@gmail.com>
2025-03-21 22:33:02 +0300

    p

--- _practice/index.js
+++ _practice/index.js
@@ -1,9 +1,22 @@
-let person={
-    name:"Adam",
-}
-console.log(person)
+// let person={
+//     name:"Adam",
+// }
+// console.log(person.toString()); 
+// Object.defineProperty(person,"name",{
+//     writable:false,
+//     configurable:true, 
+//     enumerable:true
+// })
 
-for (let key in person){
-    console.log(key);
-}
-console.log(Object.keys(person));
+// for(let key in person)
+//     console.log(key);
+
+
+// let objectBase=Object.getPrototypeOf(person);
+// const propertyDescriptor=Object.getOwnPropertyDescriptor(objectBase,"toString");
+// console.log(propertyDescriptor);
+
+function Circle(){}
+let obj={}
+console.log(obj.__proto__)
+console.log(Object.prototype)
