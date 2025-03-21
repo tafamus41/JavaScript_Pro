22fe0bfeb85182d0081fa389cd5bed4106a0afa2
asimov <seyranmustafa4@gmail.com>
2025-03-22 00:33:28 +0300

    p

---
+++ 3 Prototypes/5-constructor-prototypes.js
@@ -0,0 +1,11 @@
+function Circle() {
+    // constructor code
+}
+
+console.log(Circle.prototype);
+
+let obj = {};
+// let obj = new Object();
+
+console.log(obj.__proto__);
+console.log(Object.prototype);
