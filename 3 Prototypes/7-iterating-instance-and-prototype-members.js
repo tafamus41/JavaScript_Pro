e167bb530195a5537fd1b038923ba1d5bbeb4cb2
asimov <seyranmustafa4@gmail.com>
2025-03-22 00:34:31 +0300

    p

---
+++ 3 Prototypes/7-iterating-instance-and-prototype-members.js
@@ -0,0 +1,18 @@
+function Programmer(name, preferredLanguage) {
+    this.name = name;
+    this.preferredLanguage = preferredLanguage
+}
+
+const programmer = new Programmer('Steven', 'JavaScript');
+
+Programmer.prototype.writeCode = function() {
+    console.log(`${this.name} writes code in ${this.preferredLanguage}`)
+};
+
+programmer.writeCode();
+
+for (let key in programmer)
+    console.log(key);
+
+console.log(programmer.hasOwnProperty('name'));
+console.log(programmer.hasOwnProperty('writeCode'));
