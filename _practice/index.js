4c2cc34118c683d6d8db2528ef4dd16eceb5a23f
asimov <seyranmustafa4@gmail.com>
2025-03-24 02:08:24 +0300

    p

--- _practice/index.js
+++ _practice/index.js
@@ -1,35 +1,35 @@
-class ProgrammerUsingUnderscoreNamingConvention {
-    constructor(name, language) {
-        this._language = language;  // Not truly private, just a convention
-    }
-}
+// class ProgrammerUsingUnderscoreNamingConvention {
+//     constructor(name, language) {
+//         this._language = language;  // Not truly private, just a convention
+//     }
+// }
 
-const _language = Symbol();
+// const _language = Symbol();
 
-class ProgrammerUsingSymbols {
-    constructor(name, language) {
-        this[_language] = language;  // More private, but still accessible through reflection
-    }
-}
+// class ProgrammerUsingSymbols {
+//     constructor(name, language) {
+//         this[_language] = language;  // More private, but still accessible through reflection
+//     }
+// }
 
-const programmer = new ProgrammerUsingSymbols('Steven', 'JavaScript');
+// const programmer = new ProgrammerUsingSymbols('Steven', 'JavaScript');
 
 // Accessing the property using Symbol is not straightforward
-const languageSymbol = Object.getOwnPropertySymbols(programmer)[0];
-console.log(programmer[languageSymbol]); // Outputs the language
+//
+// console.log(programmer[languageSymbol]); // Outputs the language
 
-const _code = Symbol();
+// const _code = Symbol();
 
-class ProgrammerWithPrivateMethod {
-    constructor(name, language) {
-        this[_language] = language;
-    }
+// class ProgrammerWithPrivateMethod {
+//     constructor(name, language) {
+//         this[_language] = language;
+//     }
 
-    // Private method
-    [_code]() {
-        console.log(`${this.name} is coding in ${this[_language]}.`);
-    }
-}
+//     // Private method
+//     [_code]() {
+//         console.log(`${this.name} is coding in ${this[_language]}.`);
+//     }
+// }
 
 // Preferred ES2022 syntax with a private property and private method.
 class Programmer {
@@ -44,52 +44,55 @@ class Programmer {
         console.log(`Coding in ${this.#language}`);
     }
 }
+const programmer = new Programmer('Steven', 'JavaScript');
+console.log(programmer);
+console.log(programmer.language);
 
 /* --------------
 Exercise
 -------------- */
-class GroceryItem {
-    #name;
-    #quantity;
+// class GroceryItem {
+//     #name;
+//     #quantity;
 
-    constructor(name, quantity) {
-        this.#name = name;
-        this.#quantity = quantity;
-    }
+//     constructor(name, quantity) {
+//         this.#name = name;
+//         this.#quantity = quantity;
+//     }
 
-    // Public method to display item details
-    displayItem() {
-        return `Item: ${this.#name}, Quantity: ${this.#quantity}`
-    }
-}
+//     // Public method to display item details
+//     displayItem() {
+//         return `Item: ${this.#name}, Quantity: ${this.#quantity}`
+//     }
+// }
 
-class GroceryList {
-    #items;
+// class GroceryList {
+//     #items;
 
-    constructor() {
-        this.#items = [];
-    }
+//     constructor() {
+//         this.#items = [];
+//     }
 
-    // Public method to add an item to the list
-    addItem(name, quantity) {
-        const item = new GroceryItem(name, quantity);
-        this.#items.push(item);
-    }
+//     // Public method to add an item to the list
+//     addItem(name, quantity) {
+//         const item = new GroceryItem(name, quantity);
+//         this.#items.push(item);
+//     }
 
-    // Public method to remove an item from the list by name
-    removeItem(name) {
-        this.#items = this.#items.filter(item => !item.displayItem().includes(`Item: ${name},`));
-    }
+//     // Public method to remove an item from the list by name
+//     removeItem(name) {
+//         this.#items = this.#items.filter(item => !item.displayItem().includes(`Item: ${name},`));
+//     }
 
-    // Public method to display all items in the list
-    displayList() {
-        return this.#items.map(item => item.displayItem()).join('\n');
-    }
-}
+//     // Public method to display all items in the list
+//     displayList() {
+//         return this.#items.map(item => item.displayItem()).join('\n');
+//     }
+// }
 
-const groceryList = new GroceryList();
-groceryList.addItem('Apples', 5);
-groceryList.addItem('Bananas', 3);
-console.log(groceryList.displayList());
-groceryList.removeItem('Apples');
-console.log(groceryList.displayList());
+// const groceryList = new GroceryList();
+// groceryList.addItem('Apples', 5);
+// groceryList.addItem('Bananas', 3);
+// console.log(groceryList.displayList());
+// groceryList.removeItem('Apples');
+// console.log(groceryList.displayList());
