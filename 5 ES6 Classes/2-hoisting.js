aa648940e46974b3f3622492f2818d33e15e9373
asimov <seyranmustafa4@gmail.com>
2025-03-24 00:39:08 +0300

    p

---
+++ 5 ES6 Classes/2-hoisting.js
@@ -0,0 +1,41 @@
+const dev = new Programmer('Steven', 'JavaScript');
+
+class Programmer {
+    constructor(name, preferredLanguage) {
+        this.name = name;
+        this.preferredLanguage = preferredLanguage;
+    }
+
+    code() {
+        console.log(`${this.name} is coding in ${this.preferredLanguage}.`);
+    }
+}
+
+// Will result in a reference error
+const programmer = new MyProgrammerClass('Steven', 'JavaScript');
+programmer.code();
+
+const MyProgrammerClass = class {
+    constructor(name, preferredLanguage) {
+        this.name = name;
+        this.preferredLanguage = preferredLanguage;
+    }
+
+    code() {
+        console.log(`${this.name} is coding in ${this.preferredLanguage}.`);
+    }
+}
+
+// Will execute successfully as function declarations are hoisted
+greet();
+
+function greet() {
+    console.log('Hello World');
+}
+
+// Will result in a reference error
+sayGoodbye();
+
+const sayGoodbye = function() {
+    console.log('Goodbye');
+}
