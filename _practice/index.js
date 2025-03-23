70554940f3b357f360387500841a45d8daabd492
asimov <seyranmustafa4@gmail.com>
2025-03-24 01:01:57 +0300

    p

--- _practice/index.js
+++ _practice/index.js
@@ -1,43 +1,46 @@
-// const dev = new Programmer('Steven', 'JavaScript');
-
-// class Programmer {
-//     constructor(name, preferredLanguage) {
-//         this.name = name;
-//         this.preferredLanguage = preferredLanguage;
-//     }
-
-//     code() {
-//         console.log(`${this.name} is coding in ${this.preferredLanguage}.`);
-//     }
-// }
-
-// Will result in a reference error
-// const programmer = new MyProgrammerClass('Steven', 'JavaScript');
-// programmer.code();
-
-const MyProgrammerClass = class {
+class Programmer {
     constructor(name, preferredLanguage) {
         this.name = name;
         this.preferredLanguage = preferredLanguage;
     }
-
+    
     code() {
         console.log(`${this.name} is coding in ${this.preferredLanguage}.`);
     }
+
+    static compareSkill(programmer1, programmer2) {
+        return programmer1.preferredLanguage === programmer2.preferredLanguage;
+    }
 }
 
-// Will execute successfully as function declarations are hoisted
-// greet();
+const dev = new Programmer('Steven', 'JavaScript');
+const dev2 = new Programmer('Alice', 'JavaScript');
+console.log(Programmer.compareSkill(dev, dev2));
 
-// function greet() {
-//     console.log('Hello World');
-// }
+/* --------------
+Exercise
+-------------- */
+class GroceryItem {
+    constructor(name, quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
 
-// Will result in a reference error
-// sayGoodbye();
+    display() {
+        console.log(`${this.name} x ${this.quantity}`);
+    }
 
-// const sayGoodbye = function() {
-//     console.log('Goodbye');
-// }
+    static compareQuantities(item1, item2) {
+        if (item1.quantity > item2.quantity) {
+            console.log(`${item1.name} has more quantity than ${item2.name}`);
+        } else if (item1.quantity < item2.quantity) {
+            console.log(`${item2.name} has more quantity than ${item1.name}`);
+        } else {
+            console.log(`${item1.name} and ${item2.name} have the same quantity`);
+        }
+    }
+}
 
-// sayGoodbye();
+const apple = new GroceryItem('Apple', 7);
+const banana = new GroceryItem('Banana', 7);
+console.log(GroceryItem.compareQuantities(apple, banana));
