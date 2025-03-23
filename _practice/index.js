c02f66f6c4b6f1826a90c253fbcc49d8fcd3fd8e
asimov <seyranmustafa4@gmail.com>
2025-03-24 01:33:47 +0300

    p

--- _practice/index.js
+++ _practice/index.js
@@ -1,24 +1,24 @@
-// 'use strict';
+'use strict';
 
-function Programmer(name) {
-    this.name = name;
-    this.code = function() {
-        console.log(this);
-    }
-}
-
-// class Programmer {
-//     constructor(name) {
-//         this.name = name;
-//     }
-
-//     code() {
+// function Programmer(name) {
+//     this.name = name;
+//     this.code = function() {
 //         console.log(this);
 //     }
 // }
+
+class Programmer {
+    constructor(name) {
+        this.name = name;
+    }
+
+    code() {
+        console.log(this);
+    }
+}
 
 const programmer = new Programmer('Steven');
 programmer.code();
 
 const detachedCode = programmer.code;
-// detachedCode();
+detachedCode();
