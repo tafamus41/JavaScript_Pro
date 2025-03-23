e4bb2b3ed4ce83cbeec28412053fda425654ce59
asimov <seyranmustafa4@gmail.com>
2025-03-24 00:37:40 +0300

    p

--- _practice/index.js
+++ _practice/index.js
@@ -7,45 +7,46 @@ function ProgrammerFunc(name, preferredLanguage) {
     }
 }
 
-class Programmer {
-    constructor(name, preferredLanguage) {
-        this.name = name;
-        this.preferredLanguage = preferredLanguage;
-    }
+// class Programmer {
+//     constructor(name, preferredLanguage) {
+//         this.name = name;
+//         this.preferredLanguage = preferredLanguage;
+        
+//     }
 
-    code() {
-        console.log(`${this.name} is coding in ${this.preferredLanguage}.`);
-    }
-}
+//     code() {
+//         console.log(`${this.name} is coding in ${this.preferredLanguage}.`);
+//     }
+// }
 
-const programmer = new Programmer('Steven', 'JavaScript');
+const programmer = new ProgrammerFunc('Steven', 'JavaScript');
 programmer.code();
 
-console.log(typeof Programmer); // Outputs: function
+console.log(typeof ProgrammerFunc); // Outputs: function
 
 /* --------------
 Exercise
 -------------- */
-function GroceryItemFunc(name, quantity) {
-    this.name = name;
-    this.quantity = quantity;
+// function GroceryItemFunc(name, quantity) {
+//     this.name = name;
+//     this.quantity = quantity;
 
-    this.display = function() {
-        console.log(`${this.name} x ${this.quantity}`);
-    }
-}
+//     this.display = function() {
+//         console.log(`${this.name} x ${this.quantity}`);
+//     }
+// }
 
-class GroceryItem {
-    constructor(name, quantity) {
-        this.name = name;
-        this.quantity = quantity;
-    }
+// class GroceryItem {
+//     constructor(name, quantity) {
+//         this.name = name;
+//         this.quantity = quantity;
+//     }
 
-    display() {
-        console.log(`${this.name} x ${this.quantity}`);
-    }
-}
+//     display() {
+//         console.log(`${this.name} x ${this.quantity}`);
+//     }
+// }
 
-const item = new GroceryItem('Apple', 4);
-item.display();
+// const item = new GroceryItem('Apple', 4);
+// item.display();
 
