568d222b47d2c715e63025b9eef17b2473223693
asimov <seyranmustafa4@gmail.com>
2025-03-24 01:24:59 +0300

    p

--- _practice/index.js
+++ _practice/index.js
@@ -1,48 +1,24 @@
-class Programmer {
-    constructor(name, preferredLanguage) {
-        this.name = name;
-        this.preferredLanguage = preferredLanguage;
-    }
-    
-    code() {
-        console.log(`${this.name} is coding in ${this.preferredLanguage}.`);
-    }
+// 'use strict';
 
-    static compareSkill(programmer1, programmer2) {
-        return programmer1.preferredLanguage === programmer2.preferredLanguage;
+function Programmer(name) {
+    this.name = name;
+    this.code = function() {
+        console.log(this);
     }
 }
 
-const dev = new Programmer('Steven', 'JavaScript');
-dev.code();
-const dev2 = new Programmer('Alice', 'Python');
-dev2.code();
-console.log(Programmer.compareSkill(dev, dev2));
-
-/* --------------
-Exercise
--------------- */
-// class GroceryItem {
-//     constructor(name, quantity) {
+// class Programmer {
+//     constructor(name) {
 //         this.name = name;
-//         this.quantity = quantity;
 //     }
 
-//     display() {
-//         console.log(`${this.name} x ${this.quantity}`);
-//     }
-
-//     static compareQuantities(item1, item2) {
-//         if (item1.quantity > item2.quantity) {
-//             console.log(`${item1.name} has more quantity than ${item2.name}`);
-//         } else if (item1.quantity < item2.quantity) {
-//             console.log(`${item2.name} has more quantity than ${item1.name}`);
-//         } else {
-//             console.log(`${item1.name} and ${item2.name} have the same quantity`);
-//         }
+//     code() {
+//         console.log(this);
 //     }
 // }
 
-// const apple = new GroceryItem('Apple', 7);
-// const banana = new GroceryItem('Banana', 7);
-// console.log(GroceryItem.compareQuantities(apple, banana));
+const programmer = new Programmer('Steven');
+programmer.code();
+
+const detachedCode = programmer.code;
+// detachedCode();
