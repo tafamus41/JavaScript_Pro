bf37fbd76d0daaf826500ce87f017784609ea3c0
asimov <seyranmustafa4@gmail.com>
2025-03-23 22:30:12 +0300

    p

--- _practice/index.js
+++ _practice/index.js
@@ -5,7 +5,13 @@ function Programmer(name) {
 Programmer.prototype.code = function() {
     console.log(`${this.name} starts coding.`);
 };  
+
 function FrontEndProgrammer(name) {
     Programmer.call(this, name); // Inherits properties from Programmer
 }
+
+
+FrontEndProgrammer.prototype = Object.create(Programmer.prototype);
+
 const steven = new FrontEndProgrammer("Steven");
+console.log(FrontEndProgrammer.prototype.constructor === Programmer);
