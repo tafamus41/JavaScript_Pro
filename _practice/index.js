f7e11d27458445a6ae4a8deae4b1180e516b1520
asimov <seyranmustafa4@gmail.com>
2025-03-23 22:53:46 +0300

    p

--- _practice/index.js
+++ _practice/index.js
@@ -1,26 +1,42 @@
+function extend(Child, Parent) {
+    Child.prototype = Object.create(Parent.prototype);
+    Child.prototype.constructor = Child;
+}
+
 function Programmer(name) {
     this.name = name;
 }
 
-Programmer.prototype.code = function() {
-    console.log(`${this.name} starts coding.`);
+Programmer.prototype.work = function() {
+    console.log(`${this.name} is working on programming tasks.`);
 };
 
 function FrontEndProgrammer(name) {
     Programmer.call(this, name);
 }
 
-function extend(Child, Parent) {
-    Child.prototype = Object.create(Parent.prototype);
-    Child.prototype.constructor = Child;
+function BackEndProgrammer(name) {
+    Programmer.call(this, name);
 }
 
 extend(FrontEndProgrammer, Programmer);
+extend(BackEndProgrammer, Programmer);
 
-FrontEndProgrammer.prototype.code = function() {
-    Programmer.prototype.code.call(this); // Call the base implementation
-    console.log(`${this.name} is coding in HTML/CSS/JavaScript.`);
-};
+FrontEndProgrammer.prototype.work = function() {
+    console.log(`${this.name} is designing and coding the frontend.`);
+}
+
+BackEndProgrammer.prototype.work = function() {
+    console.log(`${this.name} is developing server-side logic.`);
+}
 
 const steven = new FrontEndProgrammer('Steven');
-steven.code();
+const alice = new BackEndProgrammer('Alice');
+
+const programmers = [
+    steven,
+    alice
+];
+
+for (let programmer of programmers)
+    programmer.work();
