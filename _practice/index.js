59e5999e5da9b3de964007e95867950c535db984
asimov <seyranmustafa4@gmail.com>
2025-03-23 22:48:37 +0300

    p

--- _practice/index.js
+++ _practice/index.js
@@ -10,20 +10,17 @@ function FrontEndProgrammer(name) {
     Programmer.call(this, name);
 }
 
-function BackEndProgrammer(name) {
-    Programmer.call(this, name);
-}
-
 function extend(Child, Parent) {
     Child.prototype = Object.create(Parent.prototype);
     Child.prototype.constructor = Child;
 }
 
 extend(FrontEndProgrammer, Programmer);
-extend(BackEndProgrammer, Programmer);
+
+FrontEndProgrammer.prototype.code = function() {
+    Programmer.prototype.code.call(this); // Call the base implementation
+    console.log(`${this.name} is coding in HTML/CSS/JavaScript.`);
+};
 
 const steven = new FrontEndProgrammer('Steven');
-const alice = new BackEndProgrammer('Alice');
-
 steven.code();
-alice.code();
