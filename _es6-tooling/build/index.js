66f8f1064bcb74e3be7d70c1bbde9c706313c664
asimov <seyranmustafa4@gmail.com>
2025-03-26 00:43:19 +0300

    p

---
+++ _es6-tooling/build/index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function _classCallCheck(a, n) { if (!(a instanceof n)) throw new TypeError("Cannot call a class as a function"); }
+function _defineProperties(e, r) { for (var t = 0; t < r.length; t++) { var o = r[t]; o.enumerable = o.enumerable || !1, o.configurable = !0, "value" in o && (o.writable = !0), Object.defineProperty(e, _toPropertyKey(o.key), o); } }
+function _createClass(e, r, t) { return r && _defineProperties(e.prototype, r), t && _defineProperties(e, t), Object.defineProperty(e, "prototype", { writable: !1 }), e; }
+function _classPrivateFieldInitSpec(e, t, a) { _checkPrivateRedeclaration(e, t), t.set(e, a); }
+function _checkPrivateRedeclaration(e, t) { if (t.has(e)) throw new TypeError("Cannot initialize the same private elements twice on an object"); }
+function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object.defineProperty(e, r, { value: t, enumerable: !0, configurable: !0, writable: !0 }) : e[r] = t, e; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
+function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
+function _classPrivateFieldGet(s, a) { return s.get(_assertClassBrand(s, a)); }
+function _classPrivateFieldSet(s, a, r) { return s.set(_assertClassBrand(s, a), r), r; }
+function _assertClassBrand(e, t, n) { if ("function" == typeof e ? e === t : e.has(t)) return arguments.length < 3 ? t : n; throw new TypeError("Private element is not present on this object"); }
+var _experience = /*#__PURE__*/new WeakMap();
+var Programmer = /*#__PURE__*/function () {
+  function Programmer(name, technology, experience) {
+    _classCallCheck(this, Programmer);
+    _defineProperty(this, "role", "developer");
+    _classPrivateFieldInitSpec(this, _experience, void 0);
+    this.name = name;
+    this.technology = technology;
+    _classPrivateFieldSet(_experience, this, experience);
+  }
+  return _createClass(Programmer, [{
+    key: "develop",
+    value: function develop() {
+      console.log("".concat(this.name, " develops software using ").concat(this.technology, " with ").concat(_classPrivateFieldGet(_experience, this), " years of experience"));
+    }
+  }, {
+    key: "getExperience",
+    value: function getExperience() {
+      return _classPrivateFieldGet(_experience, this);
+    }
+  }]);
+}();
