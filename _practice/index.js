1f9ef4716bb775fe01d9174ff319a6b54a70a149
asimov <seyranmustafa4@gmail.com>
2025-03-25 04:20:31 +0300

    p

--- _practice/index.js
+++ _practice/index.js
@@ -1,68 +1,31 @@
-
-const _language = new WeakMap();
-
 class Programmer {
-  constructor(name, language) {
-    this.name = name;
-    _language.set(this, language);
-  
-  Object.defineProperty(this, "language", {
-    get: function () {
-        console.log(_language.get(this));
-    //   return _language.get(this);
+    constructor(name) {
+        this.name = name;
     }
-  })}
 
-  
+    code() {
+        console.log(`${this.name} starts coding.`);
+    }
 }
 
-const programmer = new Programmer("Steven", "JavaScript");
-// console.log(programmer.getLanguage());  // Outputs: JavaScript
-// 
-programmer.language="Java"
-console.log(programmer.language)
-// class ProgrammerUsingObjectDefineProperty {
-//   constructor(name, language) {
-//     this.name = name;
-//     _language.set(this, language);
+class FrontEndProgrammer extends Programmer {
+    constructor(name, tools) {
+        // Call the superclass constructor with the name
+        super(name); 
+        // Additional property specific to FrontEndProgrammers
+        this.tools = tools;
+    }
 
-//     Object.defineProperty(this, "language", {
-//       get: function () {
-//         return _language.get(this);
-//       },
-//     });
-//   }
-// }
+    code() {
+        super.code(); // Call the generic code method from Programmer
+        console.log(`${this.name} codes with ${this.tools}.`);
+    }
 
-// const programmer2 = new ProgrammerUsingObjectDefineProperty(
-//   "Steven",
-//   "JavaScript"
-// );
-// console.log(programmer2.skills); // Outputs: JavaScript
+    design() {
+        console.log(`${this.name} also designs the user interfaces.`);
+    }
+}
 
-// // get and set operators
-// class ProgrammerUsingGettersAndSetters {
-//   constructor(name, language) {
-//     this.name = name;
-//     _language.set(this, language);
-//   }
-
-//   // Getter
-//   get language() {
-//     return _language.get(this);
-//   }
-
-//   // Setter
-//   set languge(newLanguage) {
-//     if (!newLanguage) throw new Error("Skills cannot be empty");
-//     _language.set(this, newLanguage);
-//   }
-// }
-
-// const programmer3 = new ProgrammerUsingGettersAndSetters(
-//   "Steven",
-//   "JavaScript"
-// );
-// console.log(programmer3.skills); // Outputs: JavaScript
-// programmer3.skills = "Python";
-// console.log(programmer3.skills); // Outputs: Python
+const frontEndDev = new FrontEndProgrammer('Steven', 'React');
+frontEndDev.code(); 
+frontEndDev.design();
