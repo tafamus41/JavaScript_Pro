761cc07ca8abcecc69f09565467aef7b904e50eb
asimov <seyranmustafa4@gmail.com>
2025-03-26 00:41:54 +0300

    p

---
+++ _es6-tooling/index.js
@@ -0,0 +1,12 @@
+class Programmer {  
+    role="developer";
+    #experience;
+    constructor(name,technology, experience) {    
+        this.name = name;
+        this.technology = technology;
+        this.#experience = experience;
+    }
+    develop() {
+        console.log(`${this.name} develops software using ${this.technology} with ${this.#experience} years of experience`);
+}    
+}
