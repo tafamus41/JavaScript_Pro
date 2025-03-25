ff6cc98c8395646ac3c739fca48274a774e6bec2
asimov <seyranmustafa4@gmail.com>
2025-03-25 05:28:36 +0300

    p

--- _practice/index.js
+++ _practice/index.js
@@ -1,23 +1,4 @@
-class Programmer {
-    constructor(name) {
-        this.name = name;
-    }
+import Programmer from './Programmer.js';
 
-    work() {
-        console.log(`${this.name} is solving problems.`);
-    }
-}
-
-class FrontEndProgrammer extends Programmer {
-    constructor(name) {
-        super(name); // Initialize the base class part of the object
-    }
-
-    work() {
-        super.work(); // Calls the base class method
-        console.log(`${this.name} is also designing and implementing UI Components`);
-    }
-}
-
-const steven = new FrontEndProgrammer('Steven');
-steven.work();
+const dev = new Programmer('Steven', 'JavaScript');
+dev.code();
