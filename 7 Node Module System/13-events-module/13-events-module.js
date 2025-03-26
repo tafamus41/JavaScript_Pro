461fe17276b3a58dd905dc68c4764ac8855d50da
asimov <seyranmustafa4@gmail.com>
2025-03-27 01:19:57 +0300

    p

---
+++ 7 Node Module System/13-events-module/13-events-module.js
@@ -0,0 +1,9 @@
+const EventEmitter = require('events');
+
+const emitter = new EventEmitter();
+
+emitter.on('messageLogged', (arg) => {
+    console.log('Listener called', arg);
+});
+
+emitter.emit('messageLogged', { id: 1, url: 'stevencodecraft.com'});
