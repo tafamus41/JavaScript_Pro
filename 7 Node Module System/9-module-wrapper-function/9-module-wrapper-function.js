021b503f2cffb2e345f65f711c6d4de875859f6a
asimov <seyranmustafa4@gmail.com>
2025-03-27 01:02:21 +0300

    p

--- 7 Node Module System/9-module-wrapper-function/9-module-wrapper-function.js
+++ 7 Node Module System/9-module-wrapper-function/9-module-wrapper-function.js
@@ -2,3 +2,7 @@
     const x='hello';
     console.log(x);
 })
+const path=require('path');
+const pathObject=path.parse(__filename);
+// console.log(path.basename(__filename));
+console.log(pathObject);
