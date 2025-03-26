786fe395d4f5a9076e6d472a9fafdd97891b2dbf
asimov <seyranmustafa4@gmail.com>
2025-03-26 23:55:40 +0300

    p

---
+++ 7 Node Module System/8-loading-a-module/index.js
@@ -0,0 +1,4 @@
+const logger = require('./logger');
+
+logger.log('This is a message');
+console.log(logger.endpoint);
---
+++ 7 Node Module System/8-loading-a-module/logger.js
@@ -0,0 +1,8 @@
+const url = 'http://mylogger.io/log';
+
+function log(message) {
+    console.log(message);
+}
+
+module.exports.log = log;
+module.exports.endpoint = url;
