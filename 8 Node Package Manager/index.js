59ea1cfb7d16ff7a689c65ab42dc0632d419210d
asimov <seyranmustafa4@gmail.com>
2025-03-28 01:27:32 +0300

    p

--- 8 Node Package Manager/index.js
+++ 8 Node Package Manager/index.js
@@ -1,4 +1,7 @@
 const _=require('underscore');
 
-const isEven=_.some([1,2,3,4,5], num=>num%2===0);
-console.log(isEven);
+// const isEven=_.some([1,2,3,4,5], num=>num%2===0);
+// console.log(isEven);
+
+const doesContain=_.contains([1,2,4,5], 3);
+console.log(doesContain);
