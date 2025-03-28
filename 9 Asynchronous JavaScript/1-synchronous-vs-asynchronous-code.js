4b9a02c9f8255fa8a9290fe7e657d553f3940977
asimov <seyranmustafa4@gmail.com>
2025-03-28 23:58:09 +0300

    p

---
+++ 9 Asynchronous JavaScript/1-synchronous-vs-asynchronous-code.js
@@ -0,0 +1,3 @@
+console.log('before');
+setTimeout(() => console.log('inside'), 2000);
+console.log('after');
--- 9 Asynchronous JavaScript/index.js
+++ 9 Asynchronous JavaScript/index.js
@@ -1,3 +1,9 @@
 console.log('before');
-setTimeout(() => console.log('inside'), 2000);
+console.log('after');
+
+console.log('before');
+setTimeout(() => {
+    // Simulate a call to the database
+    console.log('Reading a grocery item from a database...');
+}, 2000);
 console.log('after');
