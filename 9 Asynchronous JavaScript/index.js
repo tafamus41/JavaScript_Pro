a0163df28c3642ffd6cf783bc727ff63da84e81d
asimov <seyranmustafa4@gmail.com>
2025-03-29 02:56:51 +0300

    p

--- 9 Asynchronous JavaScript/index.js
+++ 9 Asynchronous JavaScript/index.js
@@ -1,72 +1,8 @@
-// function handleGroceryItems(list) {
-//   getGroceryItems(list.id, handleItemAvailability);
-// }
-
-// function handleItemAvailability(items) {
-//   checkItemAvailability(items[0], displayAvailability);
-// }
-
-// function displayAvailability(availability) {
-//   console.log('Availability:', availability);
-// }
-
-// console.log('Before');
-// getGroceryList(1, handleGroceryItems);
-// console.log('After');
-
-// function getGroceryList(id, callback) {
-//   setTimeout(() => {
-//       // Simulate a call to the database
-//       console.log('Fetching grocery list from the database...');
-//       callback({ id: id, name: 'Weekly Groceries' });
-//   }, 2000);
-// }
-
-// function getGroceryItems(listId, callback) {
-//   setTimeout(() => {
-//       console.log('Fetching grocery items...');
-//       callback(['Milk', 'Bread', 'Eggs']);
-//   }, 2000);
-// }
-
-// function checkItemAvailability(item, callback) {
-//   setTimeout(() => {
-//       console.log('Checking item availability...');
-//       callback({ item: item, available: true });
-//   }, 2000);
-// }
-
-console.log('Before');
-getGroceryList(1)
-.then(list => getGroceryItems(list.id))
-.then(items => checkItemAvailability(items[0]))
-.then(availability => console.log('Availability:', availability))
-.catch(err => console.error(err));
-console.log('After');
-
-function getGroceryList(id) {
-return new Promise((resolve) => {
+const p = new Promise((resolve, reject) => {
   setTimeout(() => {
-    console.log('Fetching grocery list from the database...');
-    resolve({ id: id, name: 'Weekly Groceries' });
+      reject(new Error('Something gone wrong'));
   }, 2000);
 });
-}
 
-function getGroceryItems(listId) {
-return new Promise((resolve) => {
-  setTimeout(() => {
-    console.log('Fetching grocery items...');
-    resolve(['Milk', 'Bread', 'Eggs']);
-  }, 2000);
-});
-}
-
-function checkItemAvailability(item) {
-return new Promise((resolve) => {
-  setTimeout(() => {
-    console.log('Checking item availability...');
-    resolve({ item: item, available: true });
-  }, 2000);
-});
-}
+p.then(result => console.log('Result:', result))
+.catch(err => console.log('Error:', err.message));
