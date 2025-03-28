04527418354596fc6df3c4f2598a21da680a00fc
asimov <seyranmustafa4@gmail.com>
2025-03-29 02:39:40 +0300

    p

--- 9 Asynchronous JavaScript/index.js
+++ 9 Asynchronous JavaScript/index.js
@@ -1,40 +1,40 @@
-function handleGroceryItems(list) {
-  getGroceryItems(list.id, handleItemAvailability);
-}
+// function handleGroceryItems(list) {
+//   getGroceryItems(list.id, handleItemAvailability);
+// }
 
-function handleItemAvailability(items) {
-  checkItemAvailability(items[0], displayAvailability);
-}
+// function handleItemAvailability(items) {
+//   checkItemAvailability(items[0], displayAvailability);
+// }
 
-function displayAvailability(availability) {
-  console.log('Availability:', availability);
-}
+// function displayAvailability(availability) {
+//   console.log('Availability:', availability);
+// }
 
-console.log('Before');
-getGroceryList(1, handleGroceryItems);
-console.log('After');
+// console.log('Before');
+// getGroceryList(1, handleGroceryItems);
+// console.log('After');
 
-function getGroceryList(id, callback) {
-  setTimeout(() => {
-      // Simulate a call to the database
-      console.log('Fetching grocery list from the database...');
-      callback({ id: id, name: 'Weekly Groceries' });
-  }, 2000);
-}
+// function getGroceryList(id, callback) {
+//   setTimeout(() => {
+//       // Simulate a call to the database
+//       console.log('Fetching grocery list from the database...');
+//       callback({ id: id, name: 'Weekly Groceries' });
+//   }, 2000);
+// }
 
-function getGroceryItems(listId, callback) {
-  setTimeout(() => {
-      console.log('Fetching grocery items...');
-      callback(['Milk', 'Bread', 'Eggs']);
-  }, 2000);
-}
+// function getGroceryItems(listId, callback) {
+//   setTimeout(() => {
+//       console.log('Fetching grocery items...');
+//       callback(['Milk', 'Bread', 'Eggs']);
+//   }, 2000);
+// }
 
-function checkItemAvailability(item, callback) {
-  setTimeout(() => {
-      console.log('Checking item availability...');
-      callback({ item: item, available: true });
-  }, 2000);
-}
+// function checkItemAvailability(item, callback) {
+//   setTimeout(() => {
+//       console.log('Checking item availability...');
+//       callback({ item: item, available: true });
+//   }, 2000);
+// }
 
 console.log('Before');
 getGroceryList(1)
