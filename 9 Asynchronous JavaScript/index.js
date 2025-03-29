093c9b6e71e195b1ecb96def5af78bfc9c75d54a
asimov <seyranmustafa4@gmail.com>
2025-03-29 03:12:47 +0300

    p

--- 9 Asynchronous JavaScript/index.js
+++ 9 Asynchronous JavaScript/index.js
@@ -1,8 +1,17 @@
-const p = new Promise((resolve, reject) => {
+const p1 = new Promise((resolve, reject) => {
   setTimeout(() => {
-      reject(new Error('Something gone wrong'));
+      console.log('Async operation 1...');
+      reject(new Error('unable to complete'));
   }, 2000);
 });
 
-p.then(result => console.log('Result:', result))
-.catch(err => console.log('Error:', err.message));
+const p2 = new Promise((resolve) => {
+  setTimeout(() => {
+      console.log('Async operation 2...');
+      resolve(2);
+  }, 2000);
+});
+
+Promise.race([p1, p2])
+  .then(result => console.log(result))
+  .catch(err => console.log('Error:', err.message));
